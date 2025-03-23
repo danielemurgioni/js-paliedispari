0283b4562465c35a06adcdebc588eec2049dd5c3
danielemurgioni <dashomew@gmail.com>
2025-03-23 23:20:12 +0100

    [FIX]

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// function palindromeWord(word){
-//     let result = "";
-//     for(let i=word.length -1; i>=0; i--){
-//         result+= word[i];
-//     }
-//     if(result==word){
-//         console.log(`La parola ${word} è palidroma`)
-//     }
-//     else{
-//         console.log(`La parola ${word} non è palidroma`)
-//     }
-// }
+function palindromeWord(word){
+    let result = "";
+    for(let i=word.length -1; i>=0; i--){
+        result+= word[i];
+    }
+    if(result==word){
+        console.log(`La parola ${word} è palidroma`)
+    }
+    else{
+        console.log(`La parola ${word} non è palidroma`)
+    }
+}
 
-// let inputWord = palindromeWord(prompt("Inserisci una parola palindroma"));
+let inputWord = palindromeWord(prompt("Inserisci una parola palindroma"));
 
 /* -------------------- */
 
