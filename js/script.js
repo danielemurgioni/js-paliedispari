b4f915eed108995589f4873336b50414165a48d7
danielemurgioni <dashomew@gmail.com>
2025-03-23 12:58:46 +0100

    [FEAT] Created a Prompt to choose Even or Odd and a function to check the Prompt

--- js/script.js
+++ js/script.js
@@ -1,14 +1,38 @@
-function palindromeWord(word){
-    let result = "";
-    for(let i=word.length -1; i>=0; i--){
-        result+= word[i];
+// function palindromeWord(word){
+//     let result = "";
+//     for(let i=word.length -1; i>=0; i--){
+//         result+= word[i];
+//     }
+//     if(result==word){
+//         console.log(`La parola ${word} è palidroma`)
+//     }
+//     else{
+//         console.log(`La parola ${word} non è palidroma`)
+//     }
+// }
+
+// let inputWord = palindromeWord(prompt("Inserisci una parola palindroma"));
+
+/* -------------------- */
+
+function checkEvenOrOdd(check){
+    let even = false;
+    let odd = false;
+
+    if(check.includes("Dispari") || check.includes("dispari")){
+    odd = true;
+    console.log("Hai scelto Dispari")
     }
-    if(result==word){
-        console.log(`La parola ${word} è palidroma`)
+    else if(check.includes("Pari") || check.includes("pari")){
+    even = true;
+    console.log("Hai scelto Pari")
     }
     else{
-        console.log(`La parola ${word} non è palidroma`)
+    console.log("Scelta errata, riprova")
     }
+    console.log(even, odd)
 }
 
-let inputWord = palindromeWord(prompt("Inserisci una parola palindroma"));
+let playerEvenOrOdd = prompt("Scegli Pari o Dispari");
+
+let playerChoiceCheck = checkEvenOrOdd(playerEvenOrOdd);
